c4b3d3b4caacb0c08e73bda1d5ddbc42be22da13
Yihui Yu <yihui10@ualberta.ca>
2024-11-09 22:52:48 -0700

    Add files via upload

---
+++ src/com/qqcommon/MessageType.java
@@ -0,0 +1,11 @@
+package com.qqcommon;
+
+public interface MessageType {
+
+    String MESSAGE_LOGIN_SUCCEED = "1";
+    String MESSAGE_LOGIN_FAIL = "2";
+    String MESSAGE_COMM_MESS = "3";
+    String MESSAGE_GET_ONLINE_FRIEND = "4";
+    String MESSAGE_RET_ONLINE_FRIEND = "5";
+    String MESSAGE_CLIENT_EXIT = "6";
+}
